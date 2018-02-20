309082182ef6e217377e974853fd596443f3bc23
Lukas Snoek <lukassnoek@gmail.com>
2018-02-20 09:40:14 +0100

    remove

--- normalize.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def demean(arr):
-    """Demeans an array (i.e., subtracts the mean
-    from each value in the array)."""
-
-    arr_demeaned = arr - np.mean(arr)
-    return arr_demeaned
-
