37a6b80832b873e63a8b4e0d9059acc17dc6e1d7
Lukas Snoek <lukassnoek@gmail.com>
2018-02-21 10:27:35 +0100

    Extend to 2D arrays

--- normalize.py
+++ normalize.py
@@ -1,9 +1,10 @@
 import numpy as np
 
-def demean(arr):
+def demean(arr, dim):
     """Demeans an array (i.e., subtracts the mean
-    from each value in the array)."""
+    from each value) for across rows (dim=0)
+    or columns (dim=1)."""
 
-    arr_demeaned = arr - np.mean(arr)
+    arr_demeaned = arr - np.mean(arr, axis=dim, keepdims=True)
     return arr_demeaned
 
